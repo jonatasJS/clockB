d80eaf70bf43c0a3be41e3f986e4d918ba7949e2
jonatasJS <soaresjonatas398@gmail.com>
2022-05-31 00:20:05 -0300

    initial

---
+++ temp.js
@@ -0,0 +1,28 @@
+const temperature = document.querySelector('[class="temperature"]');
+
+function getUserPosition() {
+  let url;
+  navigator.geolocation.getCurrentPosition((pos) => {
+    let lat = pos.coords.latitude;
+    let long = pos.coords.longitude;
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=95b11822eb429c84c1143a19251b1881`;
+    fetchApi(url);
+  });
+}
+
+function fetchApi(url) {
+  fetch(url)
+  .then((data) => {
+    return data.json();
+  })
+  .then((data) => {
+    let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
+    temperature.innerText = tempInCelsius;
+  })
+  .catch((err) => {
+    console.log(`Impossível acessar o OpenWeather. Verifique a sua conexão.`);
+    console.warn(err);
+  })
+}
+
+getUserPosition();
